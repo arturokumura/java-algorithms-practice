f4acbefd917d2bb4000ed753b77c6b973ab09791
arturokumura <arturokumura2008@gmail.com>
2026-03-04 15:25:55 -0300

    Square Every Digit

---
+++ src/exercicios/exercicio7.java
@@ -0,0 +1,19 @@
+// Platform: Codewars
+// Difficulty: 7kyu
+// Problem: Square Every Digit
+
+package exercicios;
+
+public class exercicio7 {
+    public int squareDigits(int n) {
+        return Integer.parseInt(
+                String.valueOf(n)
+                        .chars()
+                        .map(c -> Character.getNumericValue(c))
+                        .map(d -> d * d)
+                        .mapToObj(String::valueOf)
+                        .reduce("", String::concat)
+        );
+    }
+}
+
