ce2225a1e7bb6acf5bfab450604e4ea43e363b22
arturokumura <arturokumura2008@gmail.com>
2026-03-05 14:00:18 -0300

    Maximum product

---
+++ src/exercicios/exercicio9.java
@@ -0,0 +1,23 @@
+// Platform: Codewars
+// Difficulty: 7kyu
+// Problem: Maximum product
+
+package exercicios;
+
+import java.util.Scanner;
+
+public class exercicio9 {
+    public int adjacentElementsProduct(int[] array) {
+    int maior = array[0] * array[1];
+
+    for(int i=1;i<array.length -1 ; i++) {
+        int multiplication = array[i] * array[i + 1];
+
+        if(multiplication > maior) {
+            maior = multiplication;
+        }
+
+    }
+    return maior;
+}
+}
