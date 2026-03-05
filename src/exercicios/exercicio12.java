826f0762c5bd78b3c747675336941a7c6c2aea8a
arturokumura <arturokumura2008@gmail.com>
2026-03-05 17:22:46 -0300

    Get the middle character

---
+++ src/exercicios/exercicio12.java
@@ -0,0 +1,16 @@
+// Platform: Codewars
+// Difficulty: 7kyu
+// Problem: Get the middle character
+package exercicios;
+
+public class exercicio12 {
+    public static String getMiddle(String word) {
+        int meio = word.length() / 2;
+
+        if (word.length() % 2 == 0) {
+            return word.substring(meio - 1, meio + 1);
+        } else {
+            return word.substring(meio, meio + 1);
+        }
+    }
+}
