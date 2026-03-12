04ecd5ee7a7c2dac99ba4c19259f61f03f8c2871
arturokumura <arturokumura2008@gmail.com>
2026-03-12 11:01:28 -0300

    Equacao (loop II java)

--- src/exercicios/hackerranq/exercicio01.java
+++ src/exercicios/hackerranq/exercicio01.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
-public class exercicio1 {
+public class exercicio01 {
     public class Solution {
         public static void main(String[] args) throws IOException {
             Scanner sc = new Scanner(System.in);
