e1a6283f8fa2a234a511f5601441141e9f5afbee
arturokumura <arturokumura2008@gmail.com>
2026-03-18 14:32:25 -0300

    Int -> String

---
+++ src/exercicios/hackerranq/exercicio07.java
@@ -0,0 +1,17 @@
+package exercicios.hackerranq;
+
+import java.util.Scanner;
+
+public class exercicio07 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String s = String.valueOf(n);
+
+        if (n == Integer.parseInt(s)) {
+            System.out.println("Good job");
+        } else {
+            System.out.println("Wrong answer");
+        }
+    }
+}
